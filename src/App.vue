2e7bfd7d2070bb460e790cf3cc1a06f5fa56ad29
Almuker <73536819+Almuker@users.noreply.github.com>
2022-05-15 20:43:27 +0600

    Start and + Navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <NavigationBar />
+
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
+<script>
+import NavigationBar from '@/components/NavigationBar.vue'
+export default {
+  components: { NavigationBar }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,200;1,8..144,200&family=Roboto:wght@100;300;400;500;700&display=swap');
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="navigation">
+    <div>
+      <span class="navigation-icon">Navigation bar</span>
+      <input type="text" class="navigation-input" placeholder="Поиск">
+    </div>
+    <div>
+      <button class="navigation-button">Регистрация</button>
+      <button class="navigation-button">Вход</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navigation {
+  font-family: 'Roboto Serif', serif;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #333336;
+  padding: 25px 300px;
+  color: #ffffff;
+  font-size: 20px;
+}
+
+.navigation-input {
+  margin-left: 15px;
+  height: 35px;
+  width: 300px;
+  outline: none;
+  border-radius: 5px;
+  font-family: 'Roboto Serif', serif;
+  font-size: 15px;
+  padding: 0 5px;
+}
+
+.navigation-icon {
+  font-size: 25px;
+  font-weight: 600;
+}
+
+.navigation-button {
+  margin: 0 10px;
+  padding: 5px 15px;
+  font-family: 'Roboto Serif', serif;
+  font-size: 18px;
+  background-color: #ffffff;
+  cursor: pointer;
+  transition: 0.3s;
+  border: none;
+  border-radius: 2px;
+}
+
+.navigation-button:hover {
+  background-color: #0089D0;
+  color: #ffffff;
+}
+</style>
